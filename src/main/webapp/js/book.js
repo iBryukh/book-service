393e90184c0cef4b03b0b5ce85ab75d836e82887
Oleh Kurpiak <bborsch.24@gmail.com>
2015-03-26 15:58:49 +0200

    main page with js ready

---
+++ src/main/webapp/js/book.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Oleh Kurpiak on 3/26/2015.
+ */
+
+document.title = "some title";
+
+function on_load(){
+    var array = getJsonFromUrl();
+    for(var key in array){
+        alert(array[key]);
+    }
+}
+
