ff68e005a137959ec6185585a3b2a01127185b64
Oleh Kurpiak <borsch.24@gmail.com>
2015-04-04 22:14:03 +0300

    remove external files, add functionality to upload page

--- src/main/webapp/js/upload.js
+++ src/main/webapp/js/upload.js
@@ -1,81 +1,71 @@
 /**
  * Created by Oleh Kurpiak on 3/29/2015.
  */
-/*preview*/
-var loadFile = function(event) {
-    var output = document.getElementById('cover');
-    output.src = URL.createObjectURL(event.target.files[0]);
-};
-
-$(document).ready(function() {
-    $("a.plus").click(function(){
-        $("#quotes").append("<textarea class='multi-line' type='text'></textarea><br />");
-    });
-});
-
-
-
+var IMAGE = null;
 function init() {
     var rootpath = "https://" + window.location.host + "/_ah/api";
     gapi.client.load('bookapi', 'v1', null, rootpath);
 }
 
+
+$(document).ready(function() {
+    $('body').on('click', 'a.fa-plus', function(){
+        $('#quotes').append("<a class='fa fa-plus fa-2x add-remove' title='Add More Quotes'></a><textarea class='multi-line' type='text' placeholder='Quote'></textarea><br />");
+        $(this).removeClass('fa-plus');
+        $(this).addClass('fa-minus');
+    });
+    $('body').on('click', 'a.fa-minus', function (event) {
+        $(this).next().remove();
+        $(this).next().remove();
+        $(this).remove();
+    });
+    $('#load-file').on('change', function(event){
+        preview(this);
+    });
+    $('#submit').click(function(){
+        var request = {};
+        var author = $('#author').val();
+        var title = $('#title').val();
+        var year = $('#year').val();
+        var genre = $('#genre').val();
+        var annotation = $('#annotation').val();
+        var quotes = [];
+        if(!author || !title || !year || !annotation || !IMAGE){
+            alert("field null");
+            return;
+        }
+        alert('success');
+
+        var arrayOfQuotes = $('#quotes textarea');
+        for(var i = 0; i < arrayOfQuotes.length; ++i)
+            quotes.push(arrayOfQuotes[i].value);
+
+        request['author'] = author;
+        request['title'] = title;
+        request['quotes'] = quotes;
+        request['annotation'] = annotation;
+        request['genre'] = [];
+        request['year'] = year;
+        request['tags'] = [];
+        request['image'] = IMAGE;   
+        alert('create request');
+        gapi.client.bookapi.addBook(request).execute(onBookLoaded);
+    });
+});
+
+function preview(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#cover').attr('src', e.target.result);
+            IMAGE = e.target.result;
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+
 function onBookLoaded (response) {
-    //if(response.getTitle)
     for(var key in response)
         alert(key + " " + 	response[key]);
 }
-function loadCallback () {
-    //  ввімкни кнопки всякі
-
-    //по кнопці зробити
-    //document.getElementById('file-input').addEventListener('change',readSingleFile, false);
-    //alert(document.getElementById('file-input').files[0]);
-}
-
-function create(){
-    var request = {};
-    var author = document.getElementById('author').value;
-    if(!author){
-        alert("author null");
-        return;
-    }
-    var title = document.getElementById('title').value;
-    if(!title){
-        alert("title null");
-        return;
-    }
-    var file = document.getElementById('file-input').files[0];
-    if (!file) {
-        alert("file null");
-        return;
-    }
-    request['author'] = author;
-    request['title'] = title;
-    var quotes = document.getElementById('quotes').value;
-    if(quotes)
-        request['quotes'] = quotes;
-
-    var annotation = document.getElementById('annotation').value;
-    if(annotation)
-        request['annotation'] = annotation;
-
-    var genre = document.getElementById('genre').value;
-    if(genre)
-        request['genre'] = genre;
-
-    var year = document.getElementById('year').value;
-    if(year)
-        request['year'] = year;
-
-    for(var key in request)
-        alert(key + " " + 	request[key]);
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        var image = e.target.result;
-        request['image'] = image;
-        gapi.client.bookapi.addBook(request).execute(onBookLoaded);
-    };
-    reader.readAsDataURL(file);
-}
-
